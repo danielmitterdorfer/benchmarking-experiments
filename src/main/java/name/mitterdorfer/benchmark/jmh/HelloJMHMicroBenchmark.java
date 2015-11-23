1af32ab68291583229a86e873fc3cd81279a058d
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-11-23 16:03:49 +0100

    Update to JMH 1.9.3 and shadow plugin 1.2.2

--- src/main/java/name/mitterdorfer/benchmark/jmh/HelloJMHMicroBenchmark.java
+++ src/main/java/name/mitterdorfer/benchmark/jmh/HelloJMHMicroBenchmark.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.annotations.Benchmark;
  * This microbenchmark measures the runtime overhead of the total system. It is based on JMHSample_01_HelloWorld from JMH.
  */
 public class HelloJMHMicroBenchmark {
+    @SuppressWarnings("unused") //invoked by benchmarking framework
     @Benchmark
     public void benchmarkRuntimeOverhead() {
         //intentionally left blank
